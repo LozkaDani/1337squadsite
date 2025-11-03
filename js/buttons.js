12c9e4bcc173d65040fe51bd0447c9b9b603b5d1
Danila <banka228pro@gmail.com>
2025-11-03 16:59:07 +0300

    js files

---
+++ js/buttons.js
@@ -0,0 +1,19 @@
+document.getElementById("members").querySelector(".wh1te").addEventListener("click", function() {
+    window.location.href = "wh1teheaven.html";
+});
+
+document.getElementById("members").querySelector(".lozka_dani").addEventListener("click", function() {
+    window.location.href = "lozkadani.html";
+});
+document.querySelector(".d1stat").addEventListener("click", (event) => {
+    alert("не сделано")
+});
+document.querySelector(".skest357").addEventListener("click", (event) => {
+    alert("не сделано")
+});
+document.querySelector(".flickich_").addEventListener("click", (event) => {
+    alert("не сделано")
+});
+document.querySelector(".bduidan").addEventListener("click", (event) => {
+    alert("не сделано")
+});
---
+++ js/notpasxalka.js
@@ -0,0 +1,5 @@
+document.querySelector(".pashalka").addEventListener("click", (event) => {
+    document.body.innerHTML = '<img style="width: 100vw; height: 100vh;" src="assets/background/a.jpg" alt="">'
+	console.log("Вы нашли пасхалку! (и смех и грех...)")
+	alert("Вы нашли пасхалку! (и смех и грех...)")
+});
